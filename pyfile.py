8a85674489f82d58364bc6fcc76a3ab22ada5b11
joshofreeness <joshofreeness@gmail.com>
2014-09-06 23:32:46 +1200

    Added commenting and a little refactoring

--- pyfile.py
+++ pyfile.py
@@ -6,6 +6,8 @@ import re
 
 __author__ = 'Joshua Free'
 
+#This is a global string that represents the current directory, it is an absolute path(in terms of the program)
+#It also  always has a "-" on the end
 current_dir = '-'
 
 
@@ -20,9 +22,12 @@ def main():
     os.chdir("A2dir")
 
     while True:
+        #Get argument/present prompt
         arg = input('ffs> ')
+        #Split argument into an array of strings
         split_arg = string_to_list(arg)
 
+        #Execute methods associated to built in commands
         if split_arg[0] == commands[0]:
             execute_pwd()
         elif split_arg[0] == commands[1]:
@@ -38,9 +43,9 @@ def main():
         elif split_arg[0] == commands[6]:
             execute_create(split_arg)
         elif split_arg[0] == commands[7]:
-            execute_add()
+            execute_add(split_arg)
         elif split_arg[0] == commands[8]:
-            execute_cat()
+            execute_cat(split_arg)
         elif split_arg[0] == commands[9]:
             execute_delete(split_arg)
         elif split_arg[0] == commands[10]:
@@ -56,30 +61,43 @@ def execute_pwd():
 
 
 def execute_cd(args):
-    temp_dir = current_dir
+    #Get reference to the global variable of the current directory
     global current_dir
+    temp_dir = current_dir
     if len(args) == 1:
+        #If no arguments
         temp_dir = '-'
     elif args[1] == '..':
+        #If go up a directory
         if len(current_dir) == 1:
             pass
         else:
+            #Find where all the dashes are in the pwd
             dashes = [i.start() for i in re.finditer('-', current_dir)]
+            #Select the second to last dash
             remove_char = dashes[-2]
+            #Cut the end off the string representing the current directory
             temp_dir = temp_dir[:(remove_char - len(current_dir))]
+            #Reapped the end dash
             temp_dir += '-'
     elif args[1][0] == '-':
+        #If an absolute path
         if args[1][-1] != '-':
+            #If there is no dash on the end of the string, append it
             args[1] += '-'
         temp_dir = args[1]
     else:
+        #If a relative path
         if args[1][-1] != '-':
+            #If there is no dash on the end append it
             args[1] += '-'
         temp_dir = temp_dir + args[1]
 
     if find_folder(temp_dir):
+        #If there is a folder by the name inputted then continue
         current_dir = temp_dir
     else:
+        #Fail the cd command
         print('No such directory')
 
 
@@ -98,9 +116,11 @@ def execute_tree():
 
 
 def execute_clear():
-    # TODO: execute clear
+    #Move up a directory
     os.chdir('..')
+    #Delete the folder we were just in
     shutil.rmtree('A2dir')
+    #Replace the folder
     #Create A2dir if does not exist
     if not os.path.exists('A2dir'):
         os.makedirs('A2dir')
@@ -110,44 +130,106 @@ def execute_clear():
 
 def execute_create(args):
     if len(args) == 1:
+        #If only one input
         print('No file name')
         return
     elif args[1][-1] == '-':
+        #If given directory rather than file
         print("Invalid file name")
         return
     elif args[1][0] == '-':
+        #If absolute path
         call(['touch', './'+args[1]])
     else:
+        #If relative path, then create absolute path by appending
         file_name = current_dir + args[1]
         call(['touch', './'+file_name])
 
 
-def execute_add():
-    # TODO: execute add
-    print('add')
+def execute_add(args):
+    if len(args) == 1:
+        #If only one input
+        print('No file name')
+        return
+    elif args[1][-1] == '-':
+        #If given directory not file
+        print("Invalid file name")
+        return
+    elif len(args) < 3:
+        #If nothing was input to be added to file
+        print("No text argument given")
+        return
+    elif args[1][0] == '-':
+        #If absolute path
+        if find_file(args[1]):
+            #If file exists then append
+            with open(args[1], "a") as my_file:
+                my_file.write(" ".join(args[2:]))
+        else:
+            print('No such file')
+            return
+    else:
+        #If relative path, makeabsolute
+        file_name = current_dir + args[1]
+        if find_file(file_name):
+            #If file exists, append to it
+            with open(file_name, "a") as my_file:
+                my_file.write(" ".join(args[2:]))
+        else:
+            print('No such file')
+            return
 
 
-def execute_cat():
-    # TODO: execute cat
-    print('cat')
+def execute_cat(args):
+    if len(args) == 1:
+        #Wrong number of inputs
+        print('No file name')
+        return
+    elif args[1][-1] == '-':
+        #Dir not file
+        print("Invalid file name")
+        return
+    elif args[1][0] == '-':
+        #Absolute path
+        if find_file(args[1]):
+            with open(args[1], "r") as my_file:
+                print(my_file.read())
+        else:
+            print('No such file')
+            return
+    else:
+        #relative path
+        file_name = current_dir + args[1]
+        if find_file(file_name):
+            with open(file_name, "r") as my_file:
+                print(my_file.read())
+        else:
+            print('No such file')
+            return
 
 
 def execute_delete(args):
     if len(args) == 1:
+        #If wrong input
         print('No file name')
         return
     elif args[1][-1] == '-':
+        #If not file name
         print("Invalid file name")
         return
     elif args[1][0] == '-':
+        #If absolute
         if find_file(args[1]):
+            #If file exists, delete it
             os.remove(args[1])
         else:
             print('No such file')
             return
     else:
+        #If relative
         file_name = current_dir + args[1]
         if find_file(file_name):
+            #If file exists delete it
             os.remove(file_name)
         else:
             print('No such file')
@@ -161,20 +243,24 @@ def execute_dd():
 
 
 def execute_quit():
+    #Go back to original directory
     os.chdir('..')
     quit()
 
 
 def find_file(full_file_name):
+    #Check if it is a file
     return os.path.isfile(full_file_name)
 
 
 def find_folder(full_folder_name):
+    #Check if string is a folder
     files = os.listdir('.')
     return any(full_folder_name in file for file in files)
 
 
 def string_to_list(line):
+    #Code given by lecturer for use in assignments
     lexer = shlex.shlex(line, posix=True)
     lexer.whitespace_split = False
     lexer.wordchars += '#$+-,./?@^='
