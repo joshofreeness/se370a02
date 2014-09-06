8a8fde5b98ea52b676a12eabade5e8be3e7a623d
joshofreeness <joshofreeness@gmail.com>
2014-09-06 16:12:13 +1200

    Changed to python 3 and adding functionality

--- pyfile.py
+++ pyfile.py
@@ -1,10 +1,21 @@
 import shlex
+import os
+from subprocess import call
+import shutil
 
 __author__ = 'Joshua Free'
 
+current_dir = '-'
 
 def main():
     commands = ['pwd', 'cd', 'ls', 'rls', 'tree', 'clear', 'create', 'add', 'cat', 'delete', 'dd', 'quit']
+
+    #Create A2dir if does not exist
+    if not os.path.exists('A2dir'):
+        os.makedirs('A2dir')
+
+    #Change into A2dir
+    os.chdir("A2dir")
 
     while True:
         arg = input('ffs> ')
@@ -13,7 +24,7 @@ def main():
         if split_arg[0] == commands[0]:
             execute_pwd()
         elif split_arg[0] == commands[1]:
-            execute_cd()
+            execute_cd(split_arg)
         elif split_arg[0] == commands[2]:
             execute_ls()
         elif split_arg[0] == commands[3]:
@@ -39,13 +50,28 @@ def main():
 
 
 def execute_pwd():
-    # TODO: execute PWD
-    print('pwd')
+    print(current_dir)
 
 
-def execute_cd():
-    # TODO: execute CD
-    print('cd')
+def execute_cd(args):
+    # TODO: Check if the last if statement works
+    temp_dir = current_dir
+    global current_dir
+    if len(args)==1:
+        temp_dir = '-'
+    elif args[1][0] == '-':
+        if args[1][-1] != '-':
+            args[1] += '-'
+        temp_dir = args[1]
+    else:
+        if args[1][-1] != '-':
+            args[1] += '-'
+        temp_dir = current_dir + args[1]
+
+    if find_file(temp_dir):
+        current_dir = temp_dir
+    else:
+        print('No such directory')
 
 
 def execute_ls():
@@ -54,8 +80,7 @@ def execute_ls():
 
 
 def execute_rls():
-    # TODO: execute RLS
-    print('rls')
+    call(['ls','-l'])
 
 
 def execute_tree():
@@ -65,7 +90,14 @@ def execute_tree():
 
 def execute_clear():
     # TODO: execute clear
-    print('clear')
+    os.chdir('..')
+    shutil.rmtree('A2dir')
+    #Create A2dir if does not exist
+    if not os.path.exists('A2dir'):
+        os.makedirs('A2dir')
+    #Change into A2dir
+    os.chdir("A2dir")
+
 
 
 def execute_create():
@@ -94,7 +126,17 @@ def execute_dd():
 
 
 def execute_quit():
+    os.chdir('..')
     quit()
+
+
+def find_file(full_file_name):
+    return os.path.isfile(full_file_name)
+
+
+def find_folder(full_folder_name):
+    files = os.listdir('.')
+    return any(full_folder_name for file in files)
 
 
 def string_to_list(line):
